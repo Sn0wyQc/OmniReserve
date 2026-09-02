288d69b7ef0725b28b569e981d948f2da22ca326
Sn0wyQc <jmcastillav@hotmail.com>
2026-09-02 16:21:32 -0600

    Configuracion de Swagger en Program.cs

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,4 +22,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
 app.Run();
+
