bf02dbece5440a981ccf7746e03c6188075de7d0
Sn0wyQc <boxxito1@gmail.com>
2026-09-04 15:07:11 -0600

    Tarea 2: Hubieron ligeros cambios respecto a la key del maestro, se corrigieron las clases Inyection

---
+++ OmniReserve.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OmniReserve.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication()
+    {
+        return services;
+    }
+}
---
+++ OmniReserve.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OmniReserve.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure()
+    {
+        return services;
+    }
+}
