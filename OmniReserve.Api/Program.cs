8e881cb3b5c88a6de8f8450644175c4f7acca4d1
Sn0wyQc <boxxito1@gmail.com>
2026-09-04 15:07:11 -0600

    Se añadió DependencyInjection en Application e Infrastructure, además de configurar los builder en Program.cs

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -8,6 +8,10 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication()
+
+builder.Services.AddInfrastructure(builder.Configuration) 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
