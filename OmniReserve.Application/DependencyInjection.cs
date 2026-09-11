582c6d4a2276293d178a5c9dae3490e2ea033bab
Sn0wyQc <boxxito1@gmail.com>
2026-09-11 15:17:40 -0600

    Actividad 4: Se configuración MediatR e implementó CreateRoomCommand y Handler

--- OmniReserve.Application/DependencyInjection.cs
+++ OmniReserve.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Runtime.Serialization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace OmniReserve.Application;
 
@@ -6,6 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication()
     {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         return services;
     }
 }
---
+++ OmniReserve.Application/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using OmniReserve.Domain;
+
+namespace OmniReserve.Application;
+
+public class CreateRoomCommand : IRequest<Guid>
+{
+	public string RoomNumber { get; set; }
+	public RoomType Type { get; set; }
+	public decimal PricePerNight { get; set; }
+}
+        
