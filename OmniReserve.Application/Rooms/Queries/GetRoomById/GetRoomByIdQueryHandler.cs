1a40a194148b1f5eca869323488c7a4486d558ee
Sn0wyQc <boxxito1@gmail.com>
2026-09-11 15:32:10 -0600

    Tarea 4: Implementacion de GetRoomByIdQuery, RoomResponseDto y Handler en Application

---
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace OmniReserve.Application.Rooms.Queries.GetRoomById;
+{
+public class GetRoomByIdQueryHandler : IRequestHandler<GetRoomByIdQuery, RoomResponseDto>
+    
+	public Task<RoomResponseDto> Handle(
+		GetRoomByIdQuery request,
+		CancellationToken cancellationToken)
+	{
+		return Task.FromResult(room);
+	}
