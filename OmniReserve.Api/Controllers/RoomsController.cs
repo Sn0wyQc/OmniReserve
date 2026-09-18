c31d5d6ff3331d53402a692df4e8260903c9867d
Sn0wyQc <boxxito1@gmail.com>
2026-09-18 13:19:18 -0600

    Actividad 6: No hubieron cambios mayores respecto a la llave del profesor. Se corrigieron los using para arreglar los errores en IActionResult.

--- OmniReserve.Api/Controllers/RoomsController.cs
+++ OmniReserve.Api/Controllers/RoomsController.cs
@@ -17,10 +17,8 @@ public class RoomsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateRoom([FromBody] CreateRoomCommand command)
     {
-        // Se envía el objeto a MediatR, este buscará a su Handler
         var roomId = await _sender.Send(command);
 
-        // Se retorna HTTP 200 OK incluyendo la data en formato JSON automáticamente
         return Ok(roomId);
     }
 }
