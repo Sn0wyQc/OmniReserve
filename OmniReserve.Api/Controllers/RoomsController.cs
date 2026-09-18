ff498b941e5c5970765e44082802dbabda0115c1
Sn0wyQc <boxxito1@gmail.com>
2026-09-18 13:15:28 -0600

    Actividad 6: Se creó la clase RoomsController. Se implementó el endpoint CreateRoom utilizando MediatR para enviar el comando

---
+++ OmniReserve.Api/Controllers/RoomsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using OmniReserve.Application.Rooms.Commands.CreateRoom;
+
+namespace OmniReserve.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public RoomsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRoom([FromBody] CreateRoomCommand command)
+    {
+        // Se envía el objeto a MediatR, este buscará a su Handler
+        var roomId = await _sender.Send(command);
+
+        // Se retorna HTTP 200 OK incluyendo la data en formato JSON automáticamente
+        return Ok(roomId);
+    }
+}
--- OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdQueryValidator.cs
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdQueryValidator.cs
@@ -10,5 +10,6 @@ public class GetRoomByIdQueryValidator : AbstractValidator<GetRoomByIdQuery>
         RuleFor(x => x.RoomId)
             .NotEmpty()
             .WithMessage("El identificador de la habitación es obligatorio.");
+            //. 
     }
 }
