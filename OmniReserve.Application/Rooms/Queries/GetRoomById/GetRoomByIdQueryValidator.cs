e7207a1ad71ee790fe4c441d3d13b63af7cc7f43
Sn0wyQc <boxxito1@gmail.com>
2026-09-18 13:03:29 -0600

    Tarea 5: Se creó la clase GetRoomByIdQueryValidator. Se utilizó FluentValidation para la validación del query GetRoomById

---
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OmniReserve.Application.Rooms.Queries.GetRoomById;
+
+namespace OmniReserve.Application;
+
+public class GetRoomByIdQueryValidator : AbstractValidator<GetRoomByIdQuery>
+{
+    public GetRoomByIdQueryValidator()
+    {
+        RuleFor(x => x.RoomId)
+            .NotEmpty()
+            .WithMessage("El identificador de la habitación es obligatorio.");
+    }
+}
